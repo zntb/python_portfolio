cae7afd1455848ff414764d21e660ab17b34f96f
zntb <zntbdev@proton.me>
2025-07-11 07:50:03 +0300

    Send Email

---
+++ send_email.py
@@ -0,0 +1,20 @@
+import smtplib, ssl
+
+host = "smtp.gmail.com"
+port = 465
+
+username ="username@gmail.com"
+password = "password"
+
+receiver = "receiver@gmail.com"
+context = ssl.create_default_context()
+
+message = """\
+Subject: Test email
+Hi,
+This is a test email
+Thanks
+"""
+with smtplib.SMTP_SSL(host, port, context=context) as server:
+    server.login(username, password)
+    server.sendmail(username,receiver, message)
